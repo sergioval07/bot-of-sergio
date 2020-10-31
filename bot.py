cd8127d6d6d0c9dd4e630341e8c5f2765319a6f2
sergioval07 <60065121+sergioval07@users.noreply.github.com>
2020-10-31 15:02:37 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import time
+views = int(input('Number of views: '))
+views_time = float(input('Time for each view: '))
+
+browser = webdriver.Chrome()
+
+for i in rage(views):
+	browser.get(https://youtu.be/oOaP0hBbRJk)
+	time.sleep(view_time)
+
+browser.close()
